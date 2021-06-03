e4a4365d6be730d899e60990a8c1266b55562986
Dicky Khusnaedy <dickykhusnaedy98@gmail.com>
2021-06-03 12:10:58 +0700

    adding new routing for dashboard and user page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\API\MidtransController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,19 +16,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Homepage
 Route::get('/', function () {
-  return view('welcome');
+  return redirect()->route('dashboard');
 });
 
-Route::get('/debug-sentry', function () {
-  throw new Exception('My first Sentry error!');
-});
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-  return view('dashboard');
-})->name('dashboard');
+// Route Dashboard
+Route::prefix('dashboard')
+  ->middleware(['auth:sanctum', 'admin'])
+  ->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('users', UserController::class);
+  });
 
 // Midtrans route
 Route::get('/midtrans/success', [MidtransController::class, 'success']);
 Route::get('/midtrans/unfinish', [MidtransController::class, 'unfinish']);
 Route::get('/midtrans/error', [MidtransController::class, 'error']);
+  
+  // Route::get('/debug-sentry', function () {
+  //   throw new Exception('My first Sentry error!');
+  // });
